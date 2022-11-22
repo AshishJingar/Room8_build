c40b467960910957f25fb1bb26c82a9cd8a2ff65
AshishJingar <88542833+AshishJingar@users.noreply.github.com>
2022-11-22 15:24:06 +0530

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -7,7 +7,7 @@ const RESOURCES = {
 "assets/assets/images/logo.png": "dd0a79b686f661b9223c8625543dd603",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
-"assets/NOTICES": "997c93b6599228a5c45cc22b2997786f",
+"assets/NOTICES": "bd91f8b6c43be0cdfb884aee91601ac6",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/shaders/ink_sparkle.frag": "bcc362043e5b70c9a95a419f4496cae5",
 "canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
@@ -20,9 +20,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "718a56347db96df26320bfb90e1aa22d",
-"/": "718a56347db96df26320bfb90e1aa22d",
-"main.dart.js": "53a0512a3d327303361b61f218b7e37d",
+"index.html": "a38d8bec3a08b943824035703e167760",
+"/": "a38d8bec3a08b943824035703e167760",
+"main.dart.js": "655aca789aebef9f852bbb497adc451f",
 "manifest.json": "926149e4d03c83ca347072c891989049",
 "version.json": "49dec6419f7a8130c43b7672ef747134"
 };
